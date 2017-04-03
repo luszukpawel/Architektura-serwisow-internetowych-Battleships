cca9fcb6bd4c83722ca337791d8b22ab35026982
Unknown <luszukpawel@gmail.com>
2017-04-03 18:37:34 +0200

    feed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! :name => 'John 0', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John 1', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John A', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John B', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+User.create! :name => 'John C', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John D', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John F', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John q', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+User.create! :name => 'John w', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John e', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John r', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John t', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+User.create! :name => 'John r', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John t', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John y', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John u', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John i', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John o', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John p', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :name => 'John 9', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
